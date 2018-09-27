28403244762ee9567dc00c888676fd81aaabb35a
telenord <kuzspace@gmail.com>
2018-09-27 11:52:53 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch , Redirect} from 'react-router-dom';
 import Movie from './containers/Movie/Movie';
 import { deepOrange500 } from 'material-ui/styles/colors'
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles/index';
 import PageNotFound from './components/PageNotFound/PageNotFound';
-import Redirect from 'react-router-dom/es/Redirect';
-import {
-  AppBar, Drawer, FontIcon, IconButton, IconMenu,  MenuItem,
-} from 'material-ui';
-import Home from './containers/Home/Home';
-import {
-  makeSelectFavoriteList,
-} from './store/selectors';
-import { createStructuredSelector } from "reselect";
 
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+
+import Home from './containers/Home/Home';
+
 import FavoriteList from './containers/FavoriteList/FavoriteList';
 import Footer from './components/Footer/Footer';
-
-
+import Header from './containers/Header/Header';
 
 class App extends Component {
   constructor(properties, context) {
@@ -33,105 +24,11 @@ class App extends Component {
     })
   }
 
-  state = {
-    popoverIsOpen: false,
-    drawerIsOpen: false,
-  };
-
-  movieClickHandler = movie => {
-    this.handlePopoverClose();
-    this.props.history.push(`/movie/${movie}`)
-  };
-
-  handlePopoverOpen = (event) => {
-    event.preventDefault();
-    this.setState({
-      popoverIsOpen: true,
-      anchorEl: event.currentTarget,
-    });
-  };
-
-  handleDrawerOpen = () => {
-    this.setState({
-      drawerIsOpen: true,
-    });
-  };
-
-  handlePopoverClose = () => {
-    this.setState({
-      popoverIsOpen: false,
-    });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({
-      drawerIsOpen: false,
-    });
-  };
-
-  navItemClickHandler = url => {
-    this.handleDrawerClose();
-    this.props.history.push(url)
-  };
-
   render() {
-    const navItems = [{
-      url: '/',
-      title: 'Home'
-    },
-      {
-        url: '/favorite',
-        title: 'FavoriteList'
-      }
-    ];
-    const renderNavItems = navItems.map((item, i) =>
-       <MenuItem key={i} onClick={()=>this.navItemClickHandler(item.url)} primaryText={item.title}/>
-    );
-
-    const {favoriteList} = this.props;
-
-    let moviesItems = <MenuItem primaryText={'No favorite movies yet'}/>;
-    if (favoriteList.size) {
-      moviesItems = (favoriteList.map(movie => {
-        return <MenuItem key={movie.get('id')} primaryText={movie.get('title')}
-                         onClick={() => this.movieClickHandler(movie.get('id'))}/>
-
-      }));
-    }
-
-    const rightMenu = (
-      <IconMenu
-        iconButtonElement={
-          <IconButton>
-            <FontIcon
-              onClick={this.handlePopoverOpen}
-              style={{color: '#f44336'}}
-              className={'material-icons icon__favorite mr5'}
-            >
-              favorite
-            </FontIcon>
-          </IconButton>
-        }
-        anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-        targetOrigin={{horizontal: 'left', vertical: 'top'}}
-      >{moviesItems}</IconMenu>
-    );
-
     return (
       <MuiThemeProvider muiTheme={this.muiTheme}>
         <div className="App">
-          <AppBar
-            title="Menu"
-            iconElementRight={rightMenu}
-            onLeftIconButtonClick={this.handleDrawerOpen}
-          />
-          <Drawer
-            docked={false}
-            open={this.state.drawerIsOpen}
-            onRequestChange={this.handleDrawerClose}
-          >
-            {renderNavItems}
-          </Drawer>
+          <Header />
           <Switch>
             <Route path='/' exact component={Home}/>
             <Route path='/movie/:id' component={Movie}/>
@@ -139,20 +36,13 @@ class App extends Component {
             <Route path='/404' exact component={PageNotFound}/>
             <Redirect from='*' to='/404'/>
           </Switch>
-          <Footer/>
+          <Footer />
         </div>
       </MuiThemeProvider>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return createStructuredSelector({
-    favoriteList: makeSelectFavoriteList(),
-  });
-};
-
-
-export default withRouter(connect(mapStateToProps)(App));
+export default App;
 
 
